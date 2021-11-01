0d0e3aac1e5de18ba1c1d5d7f1bba6ebf32b239d
mozmbutt <mozmbutt8@gmail.com>
2021-11-01 18:59:54 +0500

    validation and sanitizing

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
 const crypto = require('crypto');
 const { Op } = require("sequelize");
+const { validationResult } = require("express-validator/check");
 
 
 const transporter = nodemailer.createTransport(sendGridTransport({
@@ -24,7 +25,12 @@ exports.login = (req, res, next) => {
     return res.render('auth/login', {
         pageTitle: 'Login',
         activeLogin: true,
-        errorMessage: errorMessage
+        errorMessage: errorMessage,
+        oldInputs: {
+            email: '',
+            password: ''
+        },
+        validationError: []
     });
 }
 
@@ -40,50 +46,89 @@ exports.register = (req, res, next) => {
     res.render('auth/register', {
         pageTitle: 'Register',
         activeRegister: true,
-        errorMessage: errorMessage
+        errorMessage: errorMessage,
+        oldInputs: {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationError: []
     });
 }
 
 exports.loginUser = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(422).render('auth/login', {
+            pageTitle: 'Register',
+            activeRegister: true,
+            errorMessage: errors.array()[0].msg,
+            oldInputs: {
+                email: email,
+                password: password
+            },
+            validationError: errors.array()
+        });
+    }
 
     User.findOne({
         where: {
             email: email
         }
     })
-        .then(user => {
-            if (!user) {
-                req.flash('error', 'Invalid Email Address !');
-                return res.redirect('/admin/login');
-            }
-            else {
-                bcrypt.compare(password, user.password)
-                    .then((passwordMatched) => {
-                        if (passwordMatched) {
-                            req.session.user = user;
-                            req.session.isLoggedIn = true;
-                            req.session.save(err => {
-                                console.log(err);
-                                return res.redirect('/error');
-                            });
-                            return res.redirect('/');
-                        }
-                        else {
-                            req.flash('error', 'Invalid Email or Password !');
-                            return res.redirect('/admin/login');
-                        }
-                    })
-                    .catch((err) => {
+    .then(user => {
+        if(!user){
+            return res.status(422).render('auth/login', {
+                pageTitle: 'Login',
+                activeLogin: true,
+                errorMessage: 'You may not have an account with this email.',
+                oldInputs: {
+                    email: email,
+                    password: password
+                },
+                validationError: [{ param: 'email' }]
+            });
+        }
+        else{
+            bcrypt.compare(password, user.password)
+            .then((passwordMatched) => {
+                if (passwordMatched) {
+                    req.session.user = user;
+                    req.session.isLoggedIn = true;
+                    req.session.save(err => {
                         console.log(err);
-                        return res.redirect('/admin/login');
+                        return res.redirect('/error');
                     });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+                    return res.redirect('/');
+                }
+                else {
+                    return res.status(422).render('auth/login', {
+                        pageTitle: 'Login',
+                        activeLogin: true,
+                        errorMessage: 'Invalid Password, Try Again',
+                        oldInputs: {
+                            email: email,
+                            password: password
+                        },
+                        validationError: [{param: 'password'}]
+
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.redirect('/admin/login');
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
 
 exports.registerUser = (req, res, next) => {
@@ -91,53 +136,54 @@ exports.registerUser = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
 
-    User.findOne({
-        where: { email: email }
-    })
-        .then(user => {
-            if (user) {
-                req.flash('error', 'Account already exist, Pick different e-mail !');
-                return res.redirect('/admin/register');
-            }
-            if (password !== confirmPassword) {
-                req.flash('error', 'Confirm Password Doesnt Matched !');
-                return res.redirect('/admin/register');
-            }
-            return bcrypt.hash(password, 12)
-                .then((hashedPassword) => {
-                    return User.create({
-                        username: username,
-                        email: email,
-                        password: hashedPassword
-                    })
-                })
-                .then(user => {
-                    user.createCart();
-                    req.session.isLoggedIn = false;
-                    req.session.user = null;
-                    req.session.save(err => {
-                        console.log(err);
-                        res.redirect('/error');
-                    })
-                    
-                    res.redirect('/admin/login');
-                    return transporter.sendMail({
-                        to: email,
-                        from: 'mozmbutt8@gmail.com',
-                        subject: 'Aroma - Account Created',
-                        text: 'Hi ' + username + '! Thanks for sign up with our online store.',
-                    })
-                    .catch( err => {
-                        console.log(err);
-                    })
-                    
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(422).render('auth/register', {
+            pageTitle: 'Register',
+            activeRegister: true,
+            errorMessage: errors.array()[0].msg,
+            oldInputs: {
+                username: username,
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword
+            },
+            validationError: errors.array()
+        });
+    }
+
+    return bcrypt.hash(password, 12)
+        .then((hashedPassword) => {
+            return User.create({
+                username: username,
+                email: email,
+                password: hashedPassword
+            })
         })
-        .catch(err => {
+        .then(user => {
+            user.createCart();
+            req.session.isLoggedIn = false;
+            req.session.user = null;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/error');
+            })
+
+            res.redirect('/admin/login');
+            return transporter.sendMail({
+                to: email,
+                from: 'mozmbutt8@gmail.com',
+                subject: 'Aroma - Account Created',
+                text: 'Hi ' + username + '! Thanks for sign up with our online store.',
+            })
+                .catch(err => {
+                    console.log(err);
+                })
+
+        })
+        .catch((err) => {
             console.log(err);
         });
 }
@@ -167,42 +213,42 @@ exports.getResetPassword = (req, res, next) => {
 exports.resetPassword = (req, res, next) => {
     const email = req.body.email;
     crypto.randomBytes(32, (err, buffer) => {
-        if(err){
+        if (err) {
             res.flash('error', err);
             return res.redirect('/reset-password');
         }
         const token = buffer.toString('hex');
-        User.findOne({ 
+        User.findOne({
             where: {
                 email: email
             }
         })
-        .then(user => {
-            if(!user){
-                console.log(user);
-                req.flash('error', 'No account found with this e-mail');
-                return res.redirect('/reset-password');
-            }
-            user.token = token;
-            user.token_expire = Date.now() + 3600000;
-            return user.save();
-        })
-        .then( result => {
-            res.redirect('/admin/login')
-            transporter.sendMail({
-                to: email,
-                from: 'mozmbutt8@gmail.com',
-                subject: 'Aroma - Reset Password',
-                text: `
+            .then(user => {
+                if (!user) {
+                    console.log(user);
+                    req.flash('error', 'No account found with this e-mail');
+                    return res.redirect('/reset-password');
+                }
+                user.token = token;
+                user.token_expire = Date.now() + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/admin/login')
+                transporter.sendMail({
+                    to: email,
+                    from: 'mozmbutt8@gmail.com',
+                    subject: 'Aroma - Reset Password',
+                    text: `
                     You requested to reset password.
                     Click this link to update password.
                     http://127.0.0.1:8000/new-password/${token}
                 `
+                })
             })
-        })
-        .catch((err) => {
-            
-        });
+            .catch((err) => {
+
+            });
     });
 }
 
@@ -217,26 +263,26 @@ exports.getNewPassword = (req, res, next) => {
             }
         }
     })
-    .then((user) => {
-        let errorMessage = req.flash('error');
-        if (errorMessage.length > 0) {
-            errorMessage = errorMessage[0];
-        } else {
-            errorMessage = null;
-        }
+        .then((user) => {
+            let errorMessage = req.flash('error');
+            if (errorMessage.length > 0) {
+                errorMessage = errorMessage[0];
+            } else {
+                errorMessage = null;
+            }
 
-        return res.render('auth/new-password', {
-            pageTitle: 'New Password',
-            activeLogin: true,
-            errorMessage: errorMessage,
-            userId: user.id,
-            token: token,
+            return res.render('auth/new-password', {
+                pageTitle: 'New Password',
+                activeLogin: true,
+                errorMessage: errorMessage,
+                userId: user.id,
+                token: token,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-    
+
 }
 
 exports.setNewPassword = (req, res, next) => {
@@ -246,9 +292,9 @@ exports.setNewPassword = (req, res, next) => {
     let token = req.body.token;
     let my_user;
 
-    if(password !== confirmPassword){
-        req.flash('error' , 'Password doesnt match !');
-        return res.redirect('/new-password/'+token);
+    if (password !== confirmPassword) {
+        req.flash('error', 'Password doesnt match !');
+        return res.redirect('/new-password/' + token);
     }
 
     User.findOne({
@@ -260,30 +306,30 @@ exports.setNewPassword = (req, res, next) => {
             id: userId
         }
     })
-    .then((user) => {
-        my_user = user;
-        return bcrypt.hash(password , 12);
-    })
-    .then( hashedPassword => {
-        my_user.password = hashedPassword,
-        my_user.token = undefined;
-        my_user.token_expire = undefined;
-        return my_user.save();
-    })
-    .then( result => {
-        res.redirect('/admin/login');
-        transporter.sendMail({
-            to: my_user.email,
-            from: 'mozmbutt8@gmail.com',
-            subject: 'Aroma - Password changed successfully',
-            text: `
+        .then((user) => {
+            my_user = user;
+            return bcrypt.hash(password, 12);
+        })
+        .then(hashedPassword => {
+            my_user.password = hashedPassword,
+                my_user.token = undefined;
+            my_user.token_expire = undefined;
+            return my_user.save();
+        })
+        .then(result => {
+            res.redirect('/admin/login');
+            transporter.sendMail({
+                to: my_user.email,
+                from: 'mozmbutt8@gmail.com',
+                subject: 'Aroma - Password changed successfully',
+                text: `
                 Your password has been changed successfully.
             `
+            })
         })
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .catch((err) => {
+            console.log(err);
+        });
 
 
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,61 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 const router = express.Router();
 const AuthController = require('../app/Http/Controllers/AuthController');
+const User = require("../app/Models/User");
+const bcrypt = require('bcryptjs');
 
 router.get('/login' , AuthController.login)
-router.post('/login' , AuthController.loginUser)
+router.post('/login',
+    check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email !')
+    .normalizeEmail(),
+    body('password')
+    .isLength({min: 4})
+    .isAlphanumeric()
+    .withMessage('Please enter a valid password')
+    .trim(), 
+
+ AuthController.loginUser)
+
 router.get('/register' , AuthController.register)
-router.post('/register' , AuthController.registerUser)
+router.post('/register' , 
+        [
+            check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email !')
+            .custom((value, {req}) => {
+                return User.findOne({
+                    where: { email: value }
+                })
+                .then(user => {
+                    if (user) {
+                        return Promise.reject(
+                            'Email already exist, Chose a diffrent one !'
+                        )
+                    }
+                })
+            })
+            .normalizeEmail(), 
+
+            body( 
+                'password', 
+                'Password length should be more than 8 and no special chracters allowed.'
+                )
+                .isLength({min: 5})
+                .isAlphanumeric()
+                .trim(),
+
+            body('confirmPassword')
+                .trim()
+                .custom((value , { req }) => {
+                    if( value !== req.body.password ){
+                        throw new Error('Confirm Password Doesnt Matched !');
+                    }
+                })
+        ],
+        AuthController.registerUser);
 router.post('/logout' , AuthController.logout)
 
 
